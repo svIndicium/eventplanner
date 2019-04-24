241b3aaf665f41599bd2dcc157e3ba02c21cae9a
joostlek <joostlek@outlook.com>
2019-04-24 14:38:27 +0200

    Update studytype
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/hu/indicium/eventplanner/group/StudyType.java
+++ src/main/java/hu/indicium/eventplanner/group/StudyType.java
@@ -1,6 +1,7 @@
 package hu.indicium.eventplanner.group;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -12,11 +13,26 @@ public class StudyType {
     @Column(name = "name", nullable = false)
     private String name;
 
-    @OneToMany(mappedBy = "studyType")
-    private List<Group> groups;
+    @Column(name = "shortname", nullable = false)
+    private String shortName;
 
-    public StudyType(String name) {
+    @OneToMany(mappedBy = "studyType")
+    private List<Group> groups = new ArrayList<>();
+
+    public StudyType() {
+    }
+
+    public StudyType(String name, String shortName) {
         this.name = name;
+        this.shortName = shortName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -25,5 +41,21 @@ public class StudyType {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public List<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(List<Group> groups) {
+        this.groups = groups;
     }
 }
